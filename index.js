2e721e3358225a3139abd5b31bbfe4fe1e8f20ca
NajafHussain7 <141527475+NajafHussain7@users.noreply.github.com>
2025-09-20 01:43:55 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,64 +1,96 @@
- 'use strict';
+const element = document.getElementById('myElement');
 
-function logger() {
-    console.log('My name is Najaf');
-} 
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-// WHEN WE NEED TO CALL/ OR INVOKE WE JUST WRITE//
+// const BMIMark = massMark / heightMark **2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
 
-// logger();
 
 
-function fruitProcessor(apples, oranges) {
-    const juice =`juice with ${apples} apples and ${oranges} oranges`;
-    return juice;
+// Switch Case Condition
+const day = 'monday';
+
+switch(day) {
+    case 'monday':
+        console.log('plan to learn coding');
+        console.log('plan for coding meetup');
+        break;
+
+        case 'tuesday':
+            console.log('going to gym');
+            break;
+
+        case 'wednesday':
+            console.log('preparation for exams');
+            console.log('do homework at home');
+            break;
+
+        case 'thursday':
+            console.log('go outing with friends');
+            break;
+
+            case 'friday':
+            case 'saturday':
+                console.log('ready to complete coding challenge');
+                break;
+                   
+                default: 
+                 console.log('not a valid day'); 
+
 }
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(6, 3);
-console.log(appleOrangeJuice);
-
-//FUNCTION DECLARATION
-
-function calcAge1(birthYear) {
-    return 2035 - birthYear;
+// if else condition//
+const day1 = 'wednesday';
+const day2 = 'thursday';
+if(day1 === 'wednesday') {
+    console.log('Preparation for Exams');
+} else {
+    console.log('Not A Valid Day');
 }
-const age1 = calcAge1(2001);
-
-//FUNCTION EXPRESSION
-
-const calcAge2 = function(birthYear) {
-    return 2035 - birthYear;
-}
-const age2 = calcAge2(2001);
-console.log(age1, age2);
-
-// ARROW FUNCTION 
-
-const calcAge3 = birthYear => 2035 - birthYear;
-const age3 = calcAge3(2001);
-console.log(age3);
-
-const yearsUntilRetirement = (birthYear, firstName) => {
-    const age = 2037 - birthYear;
-    const retirement = 50 - age;
-    return `${firstName} retires in ${retirement} years`;
-}
-console.log(yearsUntilRetirement(1999, harry));
-console.log(yearsUntilRetirement(1980, jonas));
-
-// FUNCTION IN FUNCTION
-
-function cutFruitPieces(fruit) {
-    return fruit * 5;
+if(day2 === 'thursday') {
+    console.log('Coding Challenge');
+} else {
+    console.log('Not A Valid Day');
 }
 
-function fruitProcessor(apples, oranges) {
-    const applePiece = cutFruitPieces(apples);
-    const orangePiece = cutFruitPieces(oranges);
+// elseif condition// with logical operator: OR
 
-    const juice = `juice with ${applePiece} piece of apple and ${orangePiece} pice of orange.`;
-    return juice; 
+if(day === 'monday') {
+        console.log('plan to learn coding'); 
+        console.log('plan for coding meetup');
+} else if(day === 'tuesday') {
+        console.log('going to gym');
+} else if(day === 'wednesday' || day === 'thursday') {
+        console.log('preparation for exams');
+        console.log('do homework at home');
+} else if(day === 'thursday') {
+        console.log('go outing with friends');
+} else if(day === 'friday' || day === 'saturday') {
+        console.log('ready to complete coding challenge');
+} else if(day === 'sunday') {
+        console.log('not a valid day');
 }
-console.log(fruitProcessor(5, 3));
+
+
+//Ternary Operators
+
+const age = 22;
+
+const drink = age >= 18? 'drink wine' : 'drink water';
+console.log(drink);
+
+console.log(`you can drink ${age >= 18 ? 'wine' : 'water'}`);
+
+//coding challenge
+
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 :
+bill * 0.2;
+console.log(`The bill was ${bill} the tip was ${tip} 
+    and the total value ${bill + tip}`);
+
+    //TOTAL VALUE WILL BE CHANGE IF THE BILL VALUE WILL BE CHANGED....
+
+    
